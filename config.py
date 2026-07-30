0c403aca20eb2ae723a6ed5ca8c1fcf996bb8cba
deboire <sdim@bk.ru>
2026-07-30 21:24:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ import os
 import telebot
 
 TOKEN = os.getenv('8935702983:AAELaMVrcO0smlq5MtxBB3A2qfInfW_pxAo')
-ADMIN_ID = int(os.getenv('848208221'))
+ADMIN_ID = int(os.getenv('ADMIN_ID', 848208221))
 bot = telebot.TeleBot(TOKEN)
