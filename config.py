0f0f7475b9acf7ece070dc1541d3426651eb6b43
deboire <sdim@bk.ru>
2026-07-30 20:51:37 +0300

    Add files via upload
    innital commit

---
+++ config.py
@@ -0,0 +1,9 @@
+"""
+Модуль для настройки Telegram-бота.
+"""
+import os
+import telebot
+
+TOKEN = os.getenv('8935702983:AAELaMVrcO0smlq5MtxBB3A2qfInfW_pxAo')
+ADMIN_ID = int(os.getenv('848208221'))
+bot = telebot.TeleBot(TOKEN)
